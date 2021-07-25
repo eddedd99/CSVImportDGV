850596443513b17cd16c37170367a50da3667763
Edd <edcruces99@gmail.com>
2021-07-25 08:53:06 -0500

    Add files via upload

---
+++ CSVImportDGV/CSVImportDGV/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace CSVImportDGV
+{
+    partial class frmImportarCSV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImportar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblRuta = new System.Windows.Forms.Label();
+            this.btnExportar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnImportar
+            // 
+            this.btnImportar.Location = new System.Drawing.Point(76, 12);
+            this.btnImportar.Name = "btnImportar";
+            this.btnImportar.Size = new System.Drawing.Size(125, 73);
+            this.btnImportar.TabIndex = 0;
+            this.btnImportar.Text = "Importar CSV";
+            this.btnImportar.UseVisualStyleBackColor = true;
+            this.btnImportar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 132);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(486, 245);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // lblRuta
+            // 
+            this.lblRuta.AutoSize = true;
+            this.lblRuta.Location = new System.Drawing.Point(12, 101);
+            this.lblRuta.Name = "lblRuta";
+            this.lblRuta.Size = new System.Drawing.Size(35, 13);
+            this.lblRuta.TabIndex = 2;
+            this.lblRuta.Text = "label1";
+            // 
+            // btnExportar
+            // 
+            this.btnExportar.Location = new System.Drawing.Point(237, 15);
+            this.btnExportar.Name = "btnExportar";
+            this.btnExportar.Size = new System.Drawing.Size(128, 70);
+            this.btnExportar.TabIndex = 3;
+            this.btnExportar.Text = "Exportar CSV";
+            this.btnExportar.UseVisualStyleBackColor = true;
+            this.btnExportar.Click += new System.EventHandler(this.btnExportar_Click);
+            // 
+            // frmImportarCSV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 389);
+            this.Controls.Add(this.btnExportar);
+            this.Controls.Add(this.lblRuta);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnImportar);
+            this.Name = "frmImportarCSV";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Importar archivo CSV";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImportar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblRuta;
+        private System.Windows.Forms.Button btnExportar;
+    }
+}
+
---
+++ CSVImportDGV/CSVImportDGV/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.FileIO; // This namespace usage is important or else TextFieldParser method will lead to error
+using System.IO;
+
+/*
+ * Proyecto: Importar/Exportar CSV a DataGridView a Excel
+ * Fecha: 25-jul-2021
+ * Funcion: Importar archivo de nombre "archivo.csv" a DataGridView y exportarlo a "output.xlsx"
+ * Desarrollador: edcruces99
+ * 
+ * Referencia:
+ * 
+ * Exportar
+ * https://www.c-sharpcorner.com/UploadFile/hrojasara/export-datagridview-to-excel-in-C-Sharp/
+ *
+ * Importar
+ * https://social.technet.microsoft.com/wiki/contents/articles/50922.c-importing-csv-content-and-populating-datagridview-in-windows-form-app-with-the-content.aspx
+ * */
+
+namespace CSVImportDGV
+{
+    public partial class frmImportarCSV : Form
+    {
+        public frmImportarCSV()
+        {
+            InitializeComponent();
+        }
+ 
+    public class ReadCSV
+    {
+        public DataTable readCSV;
+ 
+        public ReadCSV(string fileName, bool firstRowContainsFieldNames = true)
+        {
+            readCSV = GenerateDataTable(fileName, firstRowContainsFieldNames);
+        }
+ 
+        private static DataTable GenerateDataTable(string fileName, bool firstRowContainsFieldNames = true)
+        {
+            DataTable result = new DataTable();
+ 
+            if (fileName == "")
+            {
+                return result;
+            }
+ 
+            string delimiters = ",";
+            string extension = Path.GetExtension(fileName);
+ 
+            if (extension.ToLower() == "txt")
+                delimiters = "\t";
+            else if (extension.ToLower() == "csv")
+                delimiters = ",";
+ 
+            using (TextFieldParser tfp = new TextFieldParser(fileName))
+            {
+                tfp.SetDelimiters(delimiters);
+ 
+                // Get The Column Names
+                if (!tfp.EndOfData)
+                {
+                    string[] fields = tfp.ReadFields();
+ 
+                    for (int i = 0; i < fields.Count(); i++)
+                    {
+                        if (firstRowContainsFieldNames)
+                            result.Columns.Add(fields[i]);
+                        else
+                            result.Columns.Add("Col" + i);
+                    }
+ 
+                    // If first line is data then add it
+                    if (!firstRowContainsFieldNames)
+                        result.Rows.Add(fields);
+                }
+ 
+                // Get Remaining Rows from the CSV
+                while (!tfp.EndOfData)
+                    result.Rows.Add(tfp.ReadFields());
+            }
+ 
+            return result;
+        }
+    }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    lblRuta.Text = openFileDialog1.FileName;
+
+                    ReadCSV csv = new ReadCSV(lblRuta.Text.Trim());
+
+                    try
+                    {
+                        dataGridView1.DataSource = csv.readCSV;
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception(ex.Message);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+            // creating Excel Application  
+            Microsoft.Office.Interop.Excel._Application app = new Microsoft.Office.Interop.Excel.Application();
+            // creating new WorkBook within Excel application  
+            Microsoft.Office.Interop.Excel._Workbook workbook = app.Workbooks.Add(Type.Missing);
+            // creating new Excelsheet in workbook  
+            Microsoft.Office.Interop.Excel._Worksheet worksheet = null;
+            // see the excel sheet behind the program  
+            app.Visible = true;
+            // get the reference of first sheet. By default its name is Sheet1.  
+            // store its reference to worksheet  
+            worksheet = workbook.Sheets["Hoja1"];
+            worksheet = workbook.ActiveSheet;
+            // changing the name of active sheet  
+            worksheet.Name = "Exported from gridview";
+            // storing header part in Excel  
+            for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
+            {
+                worksheet.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
+            }
+            // storing Each row and column value to excel sheet  
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    worksheet.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                }
+            }
+            // save the application  
+            workbook.SaveAs(@"D:\Users\eduardo.cruces\Documents\Visual Studio 2013\Projects\CSVImportDGV\CSVImportDGV\bin\Debug\output.xlsx", Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+            // Exit from the application  
+            app.Quit();  
+        }
+    }
+}
